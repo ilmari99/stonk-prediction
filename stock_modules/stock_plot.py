5e848eaa4d1c493f0b4ae7ae36a33be83457dfe5
ilmari99 <i.vahteristo@gmail.com>
2023-11-11 21:36:42 +0200

    Now possible to use transformation (example minamxscaler) in plotting invest strat

--- stock_modules/stock_plot.py
+++ stock_modules/stock_plot.py
@@ -30,19 +30,19 @@ def plot_strategy_based_on_predictions(data, transformed_data, model, window_hou
   optimal_strat = calculate_optimal_invest_strategy(data)
   print(f"Profit on optimal strategy: {calculate_profit_on_invest_strategy(data, optimal_strat)}")
 
-  invest_strat = test_invest_strategy(data, window_hours, model)
+  invest_strat = test_invest_strategy(data, transformed_data, window_hours, model, inversion=inversion)
   print(f"Profit on invest strategy: {calculate_profit_on_invest_strategy(data, invest_strat)}")
   # Plot 6 columns from the test data, and mark predicted buys and sells
   fig, ax = plt.subplots(3,2)
   for i in range(3):
     for j in range(2):
       stock_idx = i*2+j
-      ax[i,j].plot(Y_test[:,stock_idx], label=f"True {ind_conversion[stock_idx] if ind_conversion is not None else stock_idx}")
+      ax[i,j].plot(data[:,stock_idx], label=ind_conversion[stock_idx])
       # Mark sells with red dots, and buys with green dots
-      red_dots = np.where(invest_strat[:,stock_idx] == 1)[0]
-      green_dots = np.where(invest_strat[:,stock_idx] == -1)[0]
-      ax[i,j].scatter(red_dots, Y_test[red_dots,stock_idx], color="red", label="Sell")
-      ax[i,j].scatter(green_dots, Y_test[green_dots,stock_idx], color="green", label="Buy")
+      sell_idx = np.argwhere(invest_strat[:,stock_idx] == 1)
+      buy_idx = np.argwhere(invest_strat[:,stock_idx] == -1)
+      ax[i,j].scatter(sell_idx, data[sell_idx,stock_idx], color="red", label="Sell")
+      ax[i,j].scatter(buy_idx, data[buy_idx,stock_idx], color="green", label="Buy")
       ax[i,j].legend()
   fig.suptitle("True and predicted values for 6 stocks")
   if show:
