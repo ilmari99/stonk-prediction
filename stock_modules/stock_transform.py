68d0cd0576a73256d267ae725413cf3147ef40dc
ilmari99 <i.vahteristo@gmail.com>
2023-11-25 16:13:45 +0200

    Added support for updown predictions

--- stock_modules/stock_transform.py
+++ stock_modules/stock_transform.py
@@ -5,7 +5,7 @@ Python module handling data reshaping and transformation.
 import numpy as np
 import pandas as pd
 
-def create_batch_xy(m_hours, histories_arr, overlap= False):
+def create_batch_xy(m_hours, histories_arr, y_updown=True, diff_data = True, overlap= False, output_scale = (0,1)):
     """
     Batch X data into sequences of length m_hours (from T to T+n), and Y data
     into sequences of length 1 (T+n+1)
@@ -15,6 +15,9 @@ def create_batch_xy(m_hours, histories_arr, overlap= False):
         histories_arr (numpy.ndarray): A 2D array of historical stock prices,
         where each row represents a time step and each column represents a
         different stock.
+        y_updown (bool, optional): Whether to predict the up/down movement.
+            If True, the labels will be 1 if the price goes up, and 0 if it goes down.
+            If False, the labels will be the actual price. Defaults to True.
     """
     x_matrix = []
     y_matrix = []
@@ -23,7 +26,25 @@ def create_batch_xy(m_hours, histories_arr, overlap= False):
         print(histories_arr)
     for i in range(0,histories_arr.shape[0]-m_hours,1 if overlap else m_hours):
         x_matrix.append(histories_arr[i:i+m_hours,:])
-        y_matrix.append(histories_arr[i+m_hours,:])
+        if not y_updown:
+            y_matrix.append(histories_arr[i+m_hours,:])
+        else:
+            # If the price goes up, the label is 1, otherwise it is 0
+            # If diff_data is True, the data is the difference, hence the label is 1 if the difference is positive, else 0
+            past_prices = histories_arr[i+m_hours-1,:]
+            curr_prices = histories_arr[i+m_hours,:]
+            if diff_data:
+                labels = np.sign(curr_prices)
+            else:
+                labels = np.sign(curr_prices - past_prices)
+                
+            # If the output scale is (-1,1), then set 0s to -1
+            if output_scale == (-1,1):
+                labels[labels == 0] = -1
+            # Else set -1 to 0
+            elif output_scale == (0,1):
+                labels[labels == -1] = 0
+            y_matrix.append(labels)
     x_matrix = np.array(x_matrix)
     y_matrix = np.array(y_matrix)
     print(
