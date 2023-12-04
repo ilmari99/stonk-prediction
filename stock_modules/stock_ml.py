b3a030c1c318c0260734520a80d87e2b5a73a9d3
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2023-12-04 18:31:35 +0200

    Finished building the Transformer; tests pending.

--- stock_modules/stock_ml.py
+++ stock_modules/stock_ml.py
@@ -2,29 +2,39 @@
 Python module handling model creation and training.
 """
 
-import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+
+from stock_modules.stock_transformer import (DataEmbedding,
+                                             transformer_encoder,
+                                             transformer_decoder)
+
+from pydantic import Field, PositiveInt
+from typing_extensions import Annotated
+
+UnitFloat = Annotated[float, Field(strict=True, ge=0., le=1.)]
 
 def create_updown_prediction_model_dense(m, n, output_scale=(0, 1)):
     """ Dense model"""
     if output_scale not in [(0, 1), (-1, 1)]:
         raise ValueError("output_scale must be either (0,1) or (-1,1).")
 
-    inputs = tf.keras.layers.Input(shape=(m, n))
+    inputs = layers.Input(shape=(m, n))
 
     # Normalize input data
-    x = tf.keras.layers.BatchNormalization()(inputs)
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(128, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.4)(x)
-    x = tf.keras.layers.Dense(64, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.4)(x)
-    x = tf.keras.layers.Dense(32, activation="relu")(x)
-    output = tf.keras.layers.Dense(n,
+    x = layers.BatchNormalization()(inputs)
+    x = layers.Flatten()(x)
+    x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dropout(0.4)(x)
+    x = layers.Dense(64, activation="relu")(x)
+    x = layers.Dropout(0.4)(x)
+    x = layers.Dense(32, activation="relu")(x)
+    output = layers.Dense(n,
                                    activation="tanh" if output_scale == (-1, 1)
                                    else "sigmoid")(x)
 
-    model = tf.keras.Model(inputs=inputs, outputs=output)
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    model = keras.Model(inputs=inputs, outputs=output)
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
                   loss="binary_crossentropy", metrics=["accuracy"])
     return model
 
@@ -33,44 +43,44 @@ def create_updown_prediction_model(m, n, output_scale=(0, 1)):
     if output_scale not in [(0, 1), (-1, 1)]:
         raise ValueError("output_scale must be either (0,1) or (-1,1).")
 
-    inputs = tf.keras.layers.Input(shape=(m, n))
+    inputs = layers.Input(shape=(m, n))
 
     # Normalize input data
-    x = tf.keras.layers.BatchNormalization()(inputs)
+    x = layers.BatchNormalization()(inputs)
 
     # Bidirectional LSTM layers with dropout
-    x = tf.keras.layers.Bidirectional(
-        tf.keras.layers.LSTM(128, return_sequences=True,
-                             kernel_regularizer=tf.keras.regularizers.l2(0.01))
+    x = layers.Bidirectional(
+        layers.LSTM(128, return_sequences=True,
+                             kernel_regularizer=keras.regularizers.l2(0.01))
     )(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
+    x = layers.Dropout(0.2)(x)
 
-    x = tf.keras.layers.Bidirectional(
-        tf.keras.layers.LSTM(64, return_sequences=True,
-                             kernel_regularizer=tf.keras.regularizers.l2(0.01))
+    x = layers.Bidirectional(
+        layers.LSTM(64, return_sequences=True,
+                             kernel_regularizer=keras.regularizers.l2(0.01))
     )(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
+    x = layers.Dropout(0.2)(x)
 
     # Attention layer
-    x = tf.keras.layers.Attention()([x, x, x])
+    x = layers.Attention()([x, x, x])
 
     # Flatten and add dropout
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
+    x = layers.Flatten()(x)
+    x = layers.Dropout(0.3)(x)
 
     # Dense layers
-    x = tf.keras.layers.Dense(32, activation="relu")(x)
+    x = layers.Dense(32, activation="relu")(x)
 
     # Output layer
-    outputs = tf.keras.layers.Dense(n,
+    outputs = layers.Dense(n,
                 activation="tanh" if output_scale == (-1, 1) else "sigmoid")(x)
 
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
 
     print(f"Model summary: {model.summary()}")
 
     # Compile the model
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
                   loss="binary_crossentropy", metrics=["accuracy"])
     return model
 
@@ -80,32 +90,69 @@ def create_price_prediction_model(m, n):
     outputs the predicted value for each stock in the next time step (1 x n).
     m is the batch size, and n is the number of stocks.
     """
-    inputs = tf.keras.layers.Input(shape=(m, n))
-    #x = tf.keras.layers.BatchNormalization()(inputs)
+    inputs = layers.Input(shape=(m, n))
+    #x = layers.BatchNormalization()(inputs)
     # Bidirectional LSTM layers
-    x = tf.keras.layers.Bidirectional(
-        tf.keras.layers.LSTM(64, return_sequences=True, kernel_regularizer="l2")
+    x = layers.Bidirectional(
+        layers.LSTM(64, return_sequences=True, kernel_regularizer="l2")
         )(inputs)
-    x = tf.keras.layers.Bidirectional(
-        tf.keras.layers.LSTM(16, return_sequences=True)
+    x = layers.Bidirectional(
+        layers.LSTM(16, return_sequences=True)
         )(x)
 
     # Attention layer
-    x = tf.keras.layers.Attention()([x, x, x])
+    x = layers.Attention()([x, x, x])
 
     # Flatten and add dropout
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dropout(0.5)(x)
+    x = layers.Flatten()(x)
+    x = layers.Dropout(0.5)(x)
 
     # Dense layer
-    x = tf.keras.layers.Dense(32, activation="relu")(x)
+    x = layers.Dense(32, activation="relu")(x)
 
     # Output (1,N)
-    outputs = tf.keras.layers.Dense(n, activation="linear")(x)
+    outputs = layers.Dense(n, activation="linear")(x)
 
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
 
     print(f"Model summary: {model.summary()}")
     # Compile the model
     model.compile(optimizer="adam", loss="huber")
     return model
+
+def create_transformer_model(m:PositiveInt, n:PositiveInt,
+                output_dim:PositiveInt=3,
+                head_size:PositiveInt=16,
+                num_heads:PositiveInt=16,
+                ff_dim:PositiveInt=32,
+                num_transformer_blocks:PositiveInt=1,
+                mlp_units:tuple[PositiveInt,...]=(64,),
+                dropout:UnitFloat=0.01,
+                mlp_dropout:UnitFloat=0.):
+    input_shape = (m,n)
+
+    # Input declaration -> [BxLxN, BxLx1]
+    context = keras.Input(shape=input_shape)
+    context_stamps = keras.Input(shape=(m,1))
+    inputs = keras.Input(shape=input_shape)
+    input_stamps = keras.Input(shape=(m,1))
+
+    # Encoder layers -> BxLxC
+    x = DataEmbedding(head_size)(context, context_stamps)
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    # Decoder layers -> BxLxC
+    y = DataEmbedding(head_size)(inputs, input_stamps)
+    for _ in range(num_transformer_blocks):
+        y = transformer_decoder(y, x, head_size, num_heads, ff_dim, dropout)
+
+    # Output layers -> BxNx3
+    y = layers.Conv1DTranspose(filters=n, kernel_size=3, padding="same")(y)
+    y = layers.Permute((2,1))(y)
+    for dim in mlp_units:
+        y = layers.Dense(dim, activation="relu")(y)
+        y = layers.Dropout(mlp_dropout)(y)
+    outputs = layers.Dense(output_dim, activation="linear")(y)
+
+    return keras.Model([context,context_stamps,inputs,input_stamps],outputs)
--- stock_modules/stock_transform.py
+++ stock_modules/stock_transform.py
@@ -5,7 +5,17 @@ Python module handling data reshaping and transformation.
 import numpy as np
 import pandas as pd
 
-def create_batch_xy(m_hours, histories_arr, y_updown=True, diff_data = True, overlap= False, output_scale = (0,1)):
+import tensorflow as tf
+
+from datetime import datetime
+
+from pydantic import PositiveInt, Field
+from typing_extensions import Annotated
+
+UnitFloat = Annotated[float, Field(strict=True, ge=0., le=1.)]
+
+def create_batch_xy(m_hours, histories_arr, y_updown=True,
+                    diff_data = True, overlap= False, output_scale = (0,1)):
     """
     Batch X data into sequences of length m_hours (from T to T+n), and Y data
     into sequences of length 1 (T+n+1)
@@ -16,7 +26,8 @@ def create_batch_xy(m_hours, histories_arr, y_updown=True, diff_data = True, ove
         where each row represents a time step and each column represents a
         different stock.
         y_updown (bool, optional): Whether to predict the up/down movement.
-            If True, the labels will be 1 if the price goes up, and 0 if it goes down.
+            If True, the labels will be 1 if the price goes up,
+            and 0 if it goes down.
             If False, the labels will be the actual price. Defaults to True.
     """
     x_matrix = []
@@ -30,14 +41,15 @@ def create_batch_xy(m_hours, histories_arr, y_updown=True, diff_data = True, ove
             y_matrix.append(histories_arr[i+m_hours,:])
         else:
             # If the price goes up, the label is 1, otherwise it is 0
-            # If diff_data is True, the data is the difference, hence the label is 1 if the difference is positive, else 0
+            # If diff_data is True, the data is the difference,
+            # hence the label is 1 if the difference is positive, else 0
             past_prices = histories_arr[i+m_hours-1,:]
             curr_prices = histories_arr[i+m_hours,:]
             if diff_data:
                 labels = np.sign(curr_prices)
             else:
                 labels = np.sign(curr_prices - past_prices)
-                
+
             # If the output scale is (-1,1), then set 0s to -1
             if output_scale == (-1,1):
                 labels[labels == 0] = -1
@@ -75,13 +87,18 @@ def histories_to_array(histories, get_dates=False):
     return np.array(values).T
 
 def add_time_delta_column(df : pd.DataFrame, date_col_name="date"):
-    """ Return a list of time differences between consecutive rows in the dataframe.
-    The time difference at index i, is the amount of time between the rows i and i+1.
+    """
+    Return a list of time differences between consecutive rows
+    in the dataframe.
+    The time difference at index i, is the amount of time between the rows
+    i and i+1.
     The time difference is in hours.
     """
     time_deltas = []
     for i in range(len(df)-1):
-        time_deltas.append((df[date_col_name].iloc[i+1] - df[date_col_name].iloc[i]).total_seconds()/3600)
+        time_deltas.append((
+                df[date_col_name].iloc[i+1] - df[date_col_name].iloc[i]
+            ).total_seconds()/3600)
     time_deltas.append(-1)
     df["Time Delta"] = time_deltas
     # datatype is int, since the measures are hourly
@@ -91,3 +108,53 @@ def add_time_delta_column(df : pd.DataFrame, date_col_name="date"):
     df = df[cols]
     return df
 
+def create_transformer_onehot_xy(
+        m_hours:PositiveInt, diff_data_np:np.ndarray,
+        data_np:np.ndarray, timestamps_np:np.ndarray,
+        static_thr:UnitFloat = 0.05,
+        date_format:str = "%Y-%m-%d %H:%M:%S"
+    ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+    aux_diff_np = diff_data_np[m_hours:,:]
+    aux_data_np = data_np[m_hours:,:]
+
+    x = np.empty([diff_data_np.shape[0]-m_hours,
+                    m_hours+1,
+                    diff_data_np.shape[1]],
+                  dtype=np.float32)
+    x_mark = np.empty([x.shape[0], x.shape[1], 4],
+                      dtype=np.int32)
+
+    y = np.zeros([diff_data_np.shape[0]-m_hours,
+                    diff_data_np.shape[1],
+                    3],
+                  dtype=np.float32)
+    mask = np.zeros([y.shape[0], y.shape[1]])
+
+    for t in range(x.shape[0]):
+        x[t,:,:] = diff_data_np[t:t+m_hours+1,:]
+
+        aux_ts = [datetime.strptime(ts, date_format)
+                  for ts in timestamps_np[t:t+m_hours+1]]
+        x_mark[t,:,0] = [stamp.month-1 for stamp in aux_ts]
+        x_mark[t,:,1] = [stamp.weekday() for stamp in aux_ts]
+        x_mark[t,:,2] = [stamp.day-1 for stamp in aux_ts]
+        x_mark[t,:,3] = [stamp.hour-7 for stamp in aux_ts]
+
+    # Class 0: Static
+    mask[np.abs(aux_diff_np) < static_thr*aux_data_np] = 1
+    y[:,:,0] = mask
+    mask[:,:] = 0
+
+    # Class 1: Increasing
+    mask[aux_diff_np > static_thr*aux_data_np] = 1
+    y[:,:,1] = mask
+    mask[:,:] = 0
+
+    # Class 2: Decreasing
+    mask[aux_diff_np < -static_thr*aux_data_np] = 1
+    y[:,:,2] = mask
+    mask[:,:] = 0
+
+    return (tf.convert_to_tensor(x, dtype=tf.float32),
+            tf.convert_to_tensor(x_mark, dtype=tf.int32),
+            tf.convert_to_tensor(y, dtype=tf.float32))
